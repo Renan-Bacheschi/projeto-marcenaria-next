60dd430a9faca3f706d37cc187379328482987b9
Renan-Bacheschi <renanbacheschimafra@gmail.com>
2025-08-28 18:42:35 -0300

    footer estrutura

---
+++ src/app/components/Footer.js
@@ -0,0 +1,18 @@
+const Footer = () =>{
+
+    const currentYear = new Date().getFullYear();
+
+    return (
+
+        <footer className="bg-gray-800 text-white py-8">
+
+
+
+
+
+        </footer>
+    )
+
+}
+
+export default Footer;
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import FirstComponent from "./components/FirstComponent";
+import Footer from "./components/Footer";
 export default function Home() {
   return (
+    <>
     <main className="container mx-auto p-8 bg-amber-900">
       <h1 className="text-4xl font-bold">Página Inicial da Marcenaria</h1>
-      <p className="mt-4">
-        <FirstComponent/>
-      </p>
+      <p className="mt-4">Marcenaria</p>
     </main>
+    <Footer/>
+    </>
   );
 }
 
