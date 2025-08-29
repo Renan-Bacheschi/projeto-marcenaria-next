bdb6feff671db02205bb8e279e642e51a17667fc
Renan-Bacheschi <renanbacheschimafra@gmail.com>
2025-08-29 19:55:51 -0300

    apagando img next

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -4,13 +4,13 @@ const Footer = () => {
 
     return (
 
-        <footer className="bg-gray-800 text-white py-8">
+        <footer className="bg-emerald-500 text-white py-8">
 
 
             <div className="w-full px-8 flex justify-between items-center">
 
                 <div>
-                    <p>&copy; {currentYear} Marcenaria Menozzi. Todos os direitos reservados.</p>
+                    <p>&copy; {currentYear} Marcenaria Menozzzzzzzi. Todos os direitos reservados.</p>
                 </div>
                 <div className="flex gap-4">
                     <a href="#" className="hover:text-lime-500 transition-colors">Facebook</a>
