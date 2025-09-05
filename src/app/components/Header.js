18bae11bd7602cba5ab1234b95b9752a940c23f5
Renan-Bacheschi <renanbacheschimafra@gmail.com>
2025-09-04 21:29:17 -0300

    add

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -5,17 +5,17 @@ const Header = () => {
 
 
   return (
-    <header className='hidden md:flex justify-between items-center py-4 px-8 bg-red-400 shadow-md'>
+    <header className='hidden md:flex justify-between items-center py-4 px-8 bg-red-400 shadow-md border-2'>
       <div className='logo'>
-        <link href='/' className='text-2xl font-bold text-amber-300'>
+        <link href='/' className='text-2xl font-bold  text-amber-300'>
         </link>
       </div>
 
       <nav>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+        <ul className='flex items-center space-x-8 border-2'>
+          <li>Projetos</li>
+          <li>Contato</li>
+          <li>Sobre Nós</li>
         </ul>
       </nav>
 
